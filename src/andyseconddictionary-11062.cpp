5bcb2da050a800b3be80f2c5ad7abe7ec0fb2050
Manish Kumar <manish.iitgcse@gmail.com>
2015-09-21 00:33:46 +0200

    Update andyseconddictionary-11062.cpp

--- src/andyseconddictionary-11062.cpp
+++ src/andyseconddictionary-11062.cpp
@@ -1,32 +1,3 @@
-//
-// andyfirstdictionary-10815.cpp
-// Copyright 2013, 2014 Manish Kumar (manish.iitgcse@gmail.com)
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions 
-// are met:
-// 
-//    * Redistributions of source code must retain the above copyright 
-//      notice, this list of conditions and the following disclaimer.
-//    * Redistributions in binary form must reproduce the above 
-//      copyright notice, this list of conditions and the following 
-//      disclaimer in the documentation and/or other materials provided 
-//      with the distribution.
-//
-//  This software is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this software.  If not, see <http://www.gnu.org/licenses/>.
-//
-//  Created on: Oct 20, 2013
-//      Author: Manish Kumar
-//
-
 #include <iostream>
 #include <algorithm>
 #include <iterator>
