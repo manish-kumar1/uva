50eb264b8271bd54f2bc7f1cffecdd1b5c710d14
speedster <manish.iitgcse@gmail.com>
2015-10-22 19:47:21 +0200

    minimum-land-price-11824.cpp

--- src/minimum-land-price-11824.cpp
+++ src/minimum-land-price-11824.cpp
@@ -15,26 +15,32 @@ InputIter apply_till(InputIter first, InputIter last, Functor f) {
     return it;
 }
 
+template<typename InputIter, typename OutputIter, typename UnaryFunctor>
+InputIter copy_till(InputIter first, OutputIter out, UnaryFunctor f) {
+    while (f(*first))
+        *out++ = *first++;
+
+    return first;
+}
+
 int main() {
     int kase;
     cin >> kase;
     while (kase--) {
         vector<int> prices;
-        auto it2 = back_inserter(prices);
-        for (auto it1 = istream_iterator<int>(cin); *it1 != 0;)
-            *it2++ = *it1++;
 
+        copy_till(istream_iterator<int>(cin), back_inserter(prices), [](const int _n) { return _n != 0; });
         sort(prices.begin(), prices.end(), greater<int>());
 
         int i = 1, total = 0;
-        find_first_of(prices.begin(), next(prices.begin()), prices.begin(), prices.end(), [&i, &total](const int dummy, const int _price) {
+        if (any_of(prices.begin(), prices.end(), [&i, &total](const int _price) {
             total += 2*pow(_price, i++);
-            if (total > 5000000) return true;
-            else return false;
-        });
-
-        if (total > 5000000) cout << "Too expensive" << endl;
-        else cout << total << endl;
+            return (total > 5000000) ? true : false;}))
+        {
+            cout << "Too expensive" << endl;
+        } else {
+            cout << total << endl;
+        }
     }
 
     return 0;
